1434610c1f879dde88b32c9cde753c9c68e834ba
Austin Dubina <austin.dubina@jivesoftware.com>
2016-02-20 20:29:52 -0800

    Main page displays the name of the support hero if there is one

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -10,4 +10,28 @@
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope, Entry) {
     $scope.entries = Entry.getList().$object;
+    $scope.todaysDate = new Date().toDateString();
+
+    $scope.isHeroOfTheDay = function (todaysDate, entries) {
+      console.log(entries);
+      var entryDates = entries.map(function (entry) {
+        return new Date(entry.date).toDateString();
+      });
+      return entryDates.includes(todaysDate);
+    };
+
+    $scope.heroOfTheDay = function (todaysDate, entries) {
+      var todaysHeros = [];
+      entries.forEach(function (entry) {
+        var parsedEntryDate = new Date(entry.date).toDateString();
+        if (parsedEntryDate == todaysDate && entry.date.length > 0) {
+          todaysHeros.push(entry.name);
+        }
+      });
+      if (todaysHeros.length > 0) {
+        return todaysHeros[0];
+      } else {
+        return "Anonymous"
+      }
+    };
   });
